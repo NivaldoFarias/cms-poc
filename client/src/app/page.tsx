f8c235d3ff6df46f7f307c3fc66b0c33e8c1e3a5
Nivaldo Farias <badivia.f@gmail.com>
2022-11-01 16:23:03 -0300

    feat: add Login default page; create structure and styles

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,38 @@
-import styles from './page.module.scss'
+import Image from 'next/image';
+
+import { HiMail } from 'react-icons/hi';
+import { RiLockPasswordFill } from 'react-icons/ri';
+
+import logo from "./../assets/img/semantix-logo_white.png";
+import styles from './page.module.scss';
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Faça seu login</h1>
+    <div className={styles.home}>
+      <main className={styles.container}>
+        <figure className={styles.figure}>
+          <Image
+            width={450}
+            quality={100}
+            src={logo}
+            alt="Semantix logo"
+          />
+          <figcaption>Faça seu Login na Plataforma</figcaption>
+        </figure>
+        <form className={styles.forms_container}>
+          <div className={styles.input_wrapper}>
+            <div className={styles.input_field}>
+              <HiMail className={styles.svg_icon} />
+              E-mail
+            </div>
+            <div className={styles.input_field}>
+              <RiLockPasswordFill className={styles.svg_icon} />
+              Senha
+            </div>
+          </div>
+        </form>
+      </main>
     </div>
   )
 }
