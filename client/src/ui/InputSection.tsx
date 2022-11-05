1df8e5e8ea48e1e950674de470bdaa67c2c7d2a1
Nivaldo Farias <badivia.f@gmail.com>
2022-11-05 19:56:53 -0300

    styles: reduce input fields label font-size

--- client/src/ui/InputSection.tsx
+++ client/src/ui/InputSection.tsx
@@ -36,8 +36,8 @@ export default function InputSection(props: Props) {
           type={type}
           name={name}
           value={state}
-          className={`input-field spacedout-field ${state.length > 0 ? "input-field--active" : ""}`}
-          ref={(element) => (inputRef.current[name] = element)}
+          className={`input-field ${type === "password" ? "input-spacedout-field" : ""} ${state.length > 0 ? "input-field--active" : ""}`}
+          ref={(element) => (inputRef.current[ name ] = element)}
           onChange={handleInputChange}
           onFocus={handleInputFocus}
           onBlur={handleInputBlur}
