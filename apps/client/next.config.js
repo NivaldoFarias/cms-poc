f5c365571c33ada5ec4187380ea55f8aa8c6623d
Nivaldo Farias <badivia.f@gmail.com>
2022-11-23 23:55:40 -0300

    perf[client]: iterate over web manifest file, add static imgs

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,15 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // * Recommended for the `pages` directory, default in `app`.
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
-  typescript: {
-    // ! WARN Dangerously allow production builds to successfully complete even if
-    // ! your project has type errors.
-    ignoreBuildErrors: true,
-  },
-};
 
-module.exports = nextConfig;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextSafe = require("next-safe");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa")({
+	dest: "public",
+});
+
+module.exports = withPWA({
+	reactStrictMode: true,
+	swcMinify: true,
+	experimental: {
+		appDir: true,
+	},
+	typescript: {
+		// ! Dangerously allow production builds to successfully complete even if your project has type errors.
+		ignoreBuildErrors: true,
+	},
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: nextSafe({
+					isDev: process.env.NODE_ENV !== "production",
+					contentSecurityPolicy: {
+						"img-src": ["'self'", "https://www.w3.org/2000/svg", "data:"],
+						"script-src": ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+					},
+				}),
+			},
+		];
+	},
+});
